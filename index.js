34ef356d58b57f3182103cd4a17d9dedd226da8c
Andrew Humphries <andrewhumphrieswork@outlook.com>
2022-04-05 06:38:18 -0500

    Basic html for search API

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('This is a test')
+// const testCatAPI = 'https://api.giphy.com/v1/gifs/translate?api_key=Re8JS3jj5HnRyaC0Gs1odAg1cxCjXmvP&s=cats'
+
+
+// fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=0c32baf96cab07598f9ffa6ec5612d52')
+//   .then(function(response) {
+//   })
+//   .catch(function(err) {
+//   });
+
+const img = document.querySelector('img')
+
+// fetch('https://api.giphy.com/v1/gifs/translate?api_key=Re8JS3jj5HnRyaC0Gs1odAg1cxCjXmvP&s=cats')
+//     .then(function(response) {
+//         return response.json()
+//     })
+//     .then(function(response) {
+//         img.src = response.data.images.original.url
+//     })
+//     .catch(function(err) {
+//     });
