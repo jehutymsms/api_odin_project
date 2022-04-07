c8085337e5cf6d0cb1b1a844e0a41d860eb34e12
Andrew Humphries <andrewhumphrieswork@outlook.com>
2022-04-07 06:24:37 -0500

    refactored using async function

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 const img = document.querySelector('img'),
     button = document.querySelector('button'),
-    input = document.querySelector('input')
+    input = document.querySelector('input');
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=Re8JS3jj5HnRyaC0Gs1odAg1cxCjXmvP&s=cats')
-.then(function(response) {
-    return response.json()
-})
-.then(function(response) {
-    img.src = response.data.images.original.url
-})
-.catch(function(err) {
-});
+getCats()
+
+async function getCats(){
+    let defaultSearch = 'cats';
+    if(input.value != ''){
+        defaultSearch = input.value;
+    };
+    try{
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Re8JS3jj5HnRyaC0Gs1odAg1cxCjXmvP&s=${defaultSearch}`),
+        catData = await response.json();
+
+    img.src = catData.data.images.original.url
+    }catch(err){
+        console.log('search did not return anything')
+    }
+};
 
 button.addEventListener('click', function(event){
-    let defaultSearch = 'cats'
-    if(input.value != ''){
-        defaultSearch = input.value
-    }
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Re8JS3jj5HnRyaC0Gs1odAg1cxCjXmvP&s=${defaultSearch}`)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url
-    })
-    .catch(function(err) {
-        console.log('search did not return anything')
-    });
-    event.preventDefault()
-})
+    getCats()
+    event.preventDefault();
+});
